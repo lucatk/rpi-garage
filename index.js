1f6d47b0a8aaa3f5cbb8525944a693f69a61a071
lucatk <lucakillmaier@lu-co.de>
2019-04-26 16:17:42 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,35 @@
+const config = require('./config.json')
+
+const express = require('express')
+const rpi433 = require('rpi-433')
+
+const {
+    port, password,
+    transmitterPin, pulseLength,
+    codes
+} = config
+
+const app = express()
+const rfEmitter = rpi433.emitter({
+    pin: transmitterPin,
+    pulseLength
+})
+
+app.post('/open/:id', (req, res) => {
+    const id = req.params.id.toLowerCase()
+    const code = codes[id]
+    if(req.query['password'] === password) {
+        if(code) {
+            return rfEmitter.sendCode(code)
+            .then((result) => {
+                console.log("Opening #" + id + ": ", result)
+            }, (error) => {
+                console.log("Error opening #" + id + ": ", error)
+            })
+        }
+    } else {
+        return res.status(403).end()
+    }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
