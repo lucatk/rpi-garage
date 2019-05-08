0b5ba0b797eb926343594f5b4eab9bec3e58ccd2
lucatk <lucakillmaier@lu-co.de>
2019-05-08 22:44:58 +0200

    Changed alexa to different server, added morgan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {
     enableAlexa,
-    port, password, sslCertificate, privateKey
+    port, password, sslCertificate, privateKey,
     transmitterPin, pulseLength,
     garages
 } = require('./config.json')
@@ -16,6 +16,27 @@ const rfEmitter = rpi433.emitter({
 })
 
 const app = express()
+
+app.post('/open/:id', (req, res) => {
+    const id = req.params.id.toLowerCase()
+    const { code } = garages[id]
+    if(req.query['password'] === password) {
+        if(code) {
+            return rfEmitter.sendCode(code)
+            .then((result) => {
+                console.log("Opening #" + id + ": ", result)
+                return res.status(200).end()
+            }, (error) => {
+                console.log("Error opening #" + id + ": ", error)
+                return res.status(500).end()
+            })
+        }
+    } else {
+        return res.status(403).end()
+    }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
 
 if(enableAlexa) {
     const alexa = new AlexaApp('alexa')
@@ -59,36 +80,22 @@ if(enableAlexa) {
         }
     })
 
+    const alexaExpress = express()
+
+    const morgan = require("morgan")
+    alexaExpress.use(morgan('dev'))
+
     alexa.express({
-        expressApp: app
+        expressApp: alexaExpress
     })
+
+    const fs = require('fs')
+    const key = fs.readFileSync('./' + privateKey)
+    const cert = fs.readFileSync('./' + sslCertificate)
+
+    const server = https.createServer({
+        key: key,
+        cert: alexaExpress
+    }, app)
+    server.listen(443, () => console.log(`[Alexa] Listening on port 443!`))
 }
-
-app.post('/open/:id', (req, res) => {
-    const id = req.params.id.toLowerCase()
-    const { code } = garages[id]
-    if(req.query['password'] === password) {
-        if(code) {
-            return rfEmitter.sendCode(code)
-            .then((result) => {
-                console.log("Opening #" + id + ": ", result)
-                return res.status(200).end()
-            }, (error) => {
-                console.log("Error opening #" + id + ": ", error)
-                return res.status(500).end()
-            })
-        }
-    } else {
-        return res.status(403).end()
-    }
-})
-
-const fs = require('fs')
-const key = fs.readFileSync('./' + privatKey)
-const cert = fs.readFileSync('./' + sslCertificate)
-
-const server = https.createServer({
-    key: key,
-    cert: cert
-}, app)
-server.listen(port, () => console.log(`Listening on port ${port}!`))
